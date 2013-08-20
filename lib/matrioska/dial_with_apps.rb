03844e19a58da66adfbf27e857552080aefea641
Ben Langfeld <ben@langfeld.me>
2013-08-20 13:50:57 -0300

    [FEATURE] Add implementation of #dial_with_remote apps

--- lib/matrioska/dial_with_apps.rb
+++ lib/matrioska/dial_with_apps.rb
@@ -14,5 +14,24 @@ module Matrioska
       dial.cleanup_calls
       dial.status
     end
+
+    def dial_with_remote_apps(to, options = {}, &block)
+      dial = Adhearsion::CallController::Dial::ParallelConfirmationDial.new to, options, call
+
+      dial.track_originating_call
+
+      dial.prep_calls do |new_call|
+        new_call.on_joined call do
+          runner = Matrioska::AppRunner.new new_call
+          yield runner, dial
+          runner.start
+        end
+      end
+
+      dial.place_calls
+      dial.await_completion
+      dial.cleanup_calls
+      dial.status
+    end
   end
 end
